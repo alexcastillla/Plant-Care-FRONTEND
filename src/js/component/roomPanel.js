c94449aa374ee3d0c86eae4abc1a328c9d232974
alexcastillla <alexander.zcas@gmail.com>
2020-11-18 15:58:46 +0000

    Added modal button with React Bootstrap

--- src/js/component/roomPanel.js
+++ src/js/component/roomPanel.js
@@ -1,10 +1,29 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
-export const Navbar = () => {
-    return(
-        <div>
-            <h1>Hola soy el panel de las Estancias</h1>
-        </div>
-    )
-}
+export const Pepocho = () => {
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
+	return (
+		<div>
+			<Button variant="primary" onClick={handleShow}>
+				Launch demo modal
+			</Button>
+			<Modal show={show} onHide={handleClose}>
+				<Modal.Header closeButton>
+					<Modal.Title>Modal heading</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>Woohoo, youre reading this text in a modal!</Modal.Body>
+				<Modal.Footer>
+					<Button variant="primary" onClick={handleClose}>
+						Save Changes
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		</div>
+	);
+};
--- src/js/views/userview.js
+++ src/js/views/userview.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../../styles/userview.scss";
+import { Pepocho } from "../component/roomPanel";
 
 export const Userview = () => {
 	return (
 		<div>
 			<div className="text-center mt-5 room-panel">
 				<h1>Hola soy el panel en donde van a ir las estancias!</h1>
+				<Pepocho />
 			</div>
 			<div className="text-center mt-5 card-panel">
 				<h1>Hola soy el panel en donde van a ir las plantas!</h1>
