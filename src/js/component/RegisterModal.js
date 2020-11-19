f215d294f3cfb9bec0f5803fa40742013212d4e5
aurelianvoinea6 <aurash1991@hotmail.com>
2020-11-19 14:34:42 +0000

    fixed the useState

--- src/js/component/RegisterModal.js
+++ src/js/component/RegisterModal.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { Context } from "../store/appContext";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
@@ -6,16 +7,13 @@ import "../../styles/registermodal.scss";
 
 export const Registermodal = () => {
 	const [show, setShow] = useState(false);
-
 	const handleClose = () => setShow(false);
 	const handleShow = () => setShow(true);
-	const CreateAccountForm = props => {
-		const { store, actions } = useContext(Context);
-		const [username, setUsername] = useState("");
-		const [email, setEmail] = useState("");
-		const [password, setPassword] = useState("");
-		const [location, setLocation] = useState("");
-	};
+	const { store, actions } = useContext(Context);
+	const [username, setUsername] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [location, setLocation] = useState("");
 
 	return (
 		<>
@@ -31,7 +29,7 @@ export const Registermodal = () => {
 					<Form>
 						<Form.Group controlId="formBasicusername">
 							<Form.Label>Username</Form.Label>
-							<Form.Control type="text" />
+							<Form.Control type="text" onChange={e => setUsername(e.target.value)} />
 							<Form.Label>Email address</Form.Label>
 							<Form.Control type="email" />
 							<Form.Label>Password</Form.Label>
