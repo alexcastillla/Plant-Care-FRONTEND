e67b5200591e8d2659613f20e7870d056ebb43a6
aurelianvoinea6 <aurash1991@hotmail.com>
2020-11-20 10:42:31 +0000

    addind some style ,color,and borders

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -6,13 +6,13 @@ import "../../styles/registermodal.scss";
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+		{/* // 	<h1>Hello Rigo!</h1>
+	// 	<p>
+	// 		<img src={rigoImage} />
+	// 	</p>
+	// 	<a href="#" className="btn btn-success">
+	// 		If you see this green button, bootstrap is working
+	// 	</a> */}
 		<Registermodal />
 	</div>
 );
