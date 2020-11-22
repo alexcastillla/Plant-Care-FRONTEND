0116127663f868a5ecd459cb78813d2802314116
alexcastillla <alexander.zcas@gmail.com>
2020-11-22 21:42:45 +0000

    Fixed error message

--- src/js/views/error.js
+++ src/js/views/error.js
@@ -6,15 +6,14 @@ export const Error = () => (
 	<section className="page_404">
 		<div className="container">
 			<div className="row">
-				<div className="col-sm-12 ">
+				<div className="col-sm-12">
 					<div className="text-center">
 						<div className="four_zero_four_bg">
-							<h1 className="text-center ">404</h1>
+							<p className="text-center ">404</p>
 						</div>
 						<div className="contant_box_404 mb-3">
-							<h3 className="h2">Opps! Look like you are lost</h3>
-							<h3>The page you are looking for</h3>
-							<h3>is not avaible!</h3>
+							<p>Oops!</p>
+							<p>Something is missing...</p>
 						</div>
 					</div>
 				</div>
