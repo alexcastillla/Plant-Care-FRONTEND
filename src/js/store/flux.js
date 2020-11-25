6d7f7b23f971eaa9e0da0b84a9639398c8e7e086
alexcastillla <alexander.zcas@gmail.com>
2020-11-25 15:33:50 +0000

    Added Plant Generator from flux seed data

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,16 +1,61 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
+			currentUser: [],
+			room: [
 				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
+					id: 1,
+					name_room: "Habitación",
+					plants: [
+						{
+							id: 1,
+							id_room: 1,
+							name_plant: "Rosalia",
+							tipo_plant: "Exterior",
+							grow_phase: "Germinación",
+							sensor_number: "111",
+							humidity_max_ideal: 85,
+							humidity_min_ideal: 80,
+							temperature_max_ideal: 38,
+							temperature_min_ideal: 3,
+							humidity_sensor: 83,
+							temperature_sensor: 24
+						},
+						{
+							id: 2,
+							id_room: 1,
+							name_plant: "Pepa",
+							tipo_plant: "Interior",
+							grow_phase: "Crecimiento",
+							sensor_number: "112",
+							humidity_max_ideal: 70,
+							humidity_min_ideal: 60,
+							temperature_max_ideal: 24,
+							temperature_min_ideal: 10,
+							humidity_sensor: 50,
+							temperature_sensor: 22
+						}
+					]
 				},
 				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
+					id: 2,
+					name_room: "Terraza",
+					plants: [
+						{
+							id: 3,
+							id_room: 2,
+							name_plant: "Lucia",
+							tipo_plant: "Exterior",
+							grow_phase: "Maduración",
+							sensor_number: "113",
+							humidity_max_ideal: 50,
+							humidity_min_ideal: 20,
+							temperature_max_ideal: 38,
+							temperature_min_ideal: 3,
+							humidity_sensor: 83,
+							temperature_sensor: 24
+						}
+					]
 				}
 			]
 		},
