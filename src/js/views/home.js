8dd0d4cb5b1ef0f4724ec05efc16a9a88585d485
alexcastillla <alexander.zcas@gmail.com>
2020-12-09 16:41:23 +0000

    Added some query for laptop Large size

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,4 +1,22 @@
 import React from "react";
 import "../../styles/home.scss";
+import logo1 from "../../img/photo1.png";
+import logo2 from "../../img/photo2.png";
 
-export const Home = () => <div className="home" />;
+export const Home = () => (
+	<div className="home">
+		<div>
+			<div>
+				<p className="texto-1">“Únete a la revolución de las terrazas”</p>
+				<p className="texto-2">
+					Tu primera plataforma en la que puedes conectar con tu planta de la manera más fácil y sencilla.
+				</p>
+				<img className="photo-1" src={logo1} />
+			</div>
+			<div className="second-section">
+				<img className="photo-2" src={logo2} />
+				<p className="texto-3">“Porque cada gota cuenta”</p>
+			</div>
+		</div>
+	</div>
+);
